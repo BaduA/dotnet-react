ca6797c174c8ad9d8de0f0fd8245d6fd1e47f3d6
BaduA <batualpustaguel@gmail.com>
2022-07-04 00:31:32 +0300

    End of Section 9

--- ui-react-dotnet/src/features/activities/dashboard/ActivityList.tsx
+++ ui-react-dotnet/src/features/activities/dashboard/ActivityList.tsx
@@ -1,55 +1,28 @@
 import { observer } from "mobx-react-lite";
-import React, { SyntheticEvent, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "semantic-ui-react";
+import React, { Fragment } from "react";
+import { Header } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import ActivityListItem from "./ActivityListItem";
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
-  const {deleteActivity,activitiesByDate,loading} = activityStore
+  const { groupedActivities } = activityStore;
 
-  const [target, setTarget] = useState("");
-
-  function handleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
   return (
-    <div className="activity-list-container">
-      {activitiesByDate.map((activity) => (
-        <div key={activity.id} className="activity-item">
-          <div className="activity-header">
-            <h3>{activity.title}</h3>
-            <p>{activity.date}</p>
-          </div>
-
-          <p>{activity.description}</p>
-          <p>{activity.category}</p>
-          <p>
-            {activity.city}, {activity.venue}
-          </p>
-          <div className="activity-buttons">
-            <Button
-              name={activity.id}
-              loading={loading && target === activity.id}
-              onClick={(e) => handleActivityDelete(e, activity.id)}
-              className="activity-category"
-            >
-              Delete
-            </Button>
-            <Button
-               as={Link}
-               to={`/activities/${activity.id}`}
-              className="activity-button"
-            >
-              View
-            </Button>
-          </div>
-        </div>
+    <>
+      {groupedActivities.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Header sub color="teal">
+            {group}
+          </Header>
+          {activities.map((activity) => (
+            <ActivityListItem
+              key={activity.id}
+              activity={activity}
+            ></ActivityListItem>
+          ))}
+        </Fragment>
       ))}
-    </div>
+    </>
   );
 });
--- ui-react-dotnet/src/features/home/homepage.tsx
+++ ui-react-dotnet/src/features/home/homepage.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 
 export default function HomePage() {
     return (
-        <Container style={{marginTop:"7em"}}>
-            <h1>Home Page</h1>
-            <h4>Go to <Link to="/activities">Activities</Link></h4>
-        </Container>
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as="h1" inverted>
+                    <Image size="massive" src="/assets/logo.png" style={{marginBottom:12}}></Image>
+                    React-Dotnet
+                </Header>
+                <Header as="h2" inverted content="Welcome"></Header>
+                <Button as={Link} to="/activities" size="huge" inverted>
+                    Take me to activities
+                </Button>
+            </Container>
+
+        </Segment>
     )
 }
