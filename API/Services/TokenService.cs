1f760b976c2dee543694ab0e74ab14ca11693339
BaduA <batualpustaguel@gmail.com>
2022-07-07 01:37:33 +0300

    End of Section 12

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        public IConfiguration Config { get; }
+        public TokenService(IConfiguration config)
+        {
+            this.Config = config;
+            
+        }
+        public string CreateToken(AppUser user){
+            var claims = new List<Claim>{
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Config["TokenKey"]));
+            // dotnet add package Microsoft.IdentityModel.Tokens --version 6.21.0
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
